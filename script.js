c1144cb5cba77d3bad467bab8a4262b119fe296c
samcoombs <76431238+samcoombs@users.noreply.github.com>
2021-01-06 13:20:39 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const navLink = document.querySelector(".nav__link");
+const navList = document.querySelector(".nav__list");
+
+const nav = document.querySelector(".nav__wrapper");
+const heading = document.querySelector(".heading");
+
+// SMOOTH SCROLL
+
+navList.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+// STICKY NAV
+
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(heading);
